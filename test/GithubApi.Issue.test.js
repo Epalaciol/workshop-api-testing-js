e5ab9a5db6909b866e7e98f1c3b637cdce4659ad
Epalaciol <esteban.1054@hotmail.com>
2021-02-07 23:53:15 -0500

    prueba travis

---
+++ test/GithubApi.Issue.test.js
@@ -0,0 +1,5 @@
+const agent = require('superagent');
+const statusCode = require('http-status-codes');
+const { expect/* , assert */ } = require('chai');
+
+const urlBase = 'https://api.github.com';
